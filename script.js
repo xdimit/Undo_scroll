e858b7928c2b8a796ad45c31adaf173794b207c4
xdimit <xdimit@rambler.ru>
2020-03-07 22:15:59 +0500

    Added overflow=hidden for DocumentBody

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 'use strict'
 
-document.documentElement.onmousewheel = function () {
-    this.style.overflow = 'hidden';
-};
+/*---------------mouse whell----------------*/
+
+// document.body.onmousewheel = function (e) {
+//     this.style.overflow = 'hidden';
+// };
+
+/*___________________________________________*/
+
+/*--------------key and cod -----------------*/
+
+/*-----этот можно отменить на клавишах ------*/
+
+// document.documentElement.addEventListener( 'keydown', cancelScrollKeys);
+
+// function cancelScrollKeys(e) {
+
+// 	if (e.code == 'Space' ||
+// 		e.key == 'Home' ||
+// 		e.key == 'End' ||
+// 		e.key == 'ArrowUp' ||
+// 		 e.key == 'ArrowDown' ||
+// 		 e.key == 'PageUp' ||
+// 		 e.key == 'PageDown' ) {
+
+//         e.preventDefault();
+//     }
+// };
+
+/*_____________________________________________*/
 
 
+/*------------самый надежный метод -----------*/
+document.body.style.overflow = 'hidden';
+/*____________________________________________*/
